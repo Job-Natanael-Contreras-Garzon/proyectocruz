c9f9457e1f6f6daace07c27a97f0fbc1f500fc8c
Job-Natanael-Contreras-Garzón <contrerasjob123@gmail.com>
2024-05-26 05:17:33 -0400

    alm

--- src/app/add-new-almacen/add-new-almacen.component.ts
+++ src/app/add-new-almacen/add-new-almacen.component.ts
@@ -8,22 +8,20 @@ import { Almacen } from '../interfaces/almacen';
 @Component({
   selector: 'app-add-new-almacen',
   templateUrl: './add-new-almacen.component.html',
-  styleUrl: './add-new-almacen.component.css'
+  styleUrls: ['./add-new-almacen.component.css'] // Corrección aquí
 })
-export class AddNewAlmacenComponent implements OnInit{
-  
-
+export class AddNewAlmacenComponent implements OnInit {
   almacenes: Almacen[] = []; // Aquí almacenaremos los almacenes
   AlmacenForm: FormGroup;
-  id:number;
-  operacion:string = 'Agregar ';
+  id: number;
+  operacion: string = 'Agregar ';
 
   constructor(private fb: FormBuilder,
     private _almacenServices: AlmacenServices,
     private toastr: ToastrService,
     private aRouter: ActivatedRoute,
     private router: Router,
-  ){
+  ) {
     this.AlmacenForm = this.fb.group({
       nombre: ['', Validators.required],
       direccion: ['', Validators.required],
@@ -33,60 +31,54 @@ export class AddNewAlmacenComponent implements OnInit{
     });
 
     this.id = Number(this.aRouter.snapshot.paramMap.get('id'));
-    //console.log(this.id);
-    
   }
-  
+
   ngOnInit(): void {
-    if(this.id!=0){
-      this.operacion='Modificar';
+    if (this.id != 0) {
+      this.operacion = 'Modificar';
       this.getAlmacen(this.id);
     }
   }
 
-
-  newAlmacen(){
+  newAlmacen() {
     const almacenData = this.AlmacenForm.getRawValue();
-    const almacen:Almacen = {
-      nombre: almacenData.nombre, 
+    const almacen: Almacen = {
+      nombre: almacenData.nombre,
       direccion: almacenData.direccion,
       ciudad: almacenData.ciudad,
       capacidad_actual: almacenData.capacidad_actual,
       capacidad_total: almacenData.capacidad_total
     }
 
-    if(this.id!=0){
-      //modificar
-      almacen.id=this.id;
-      this._almacenServices.UpdateAlmacen(this.id,almacen).subscribe(()=>{
-        this.toastr.success(`El Proveedor ${almacenData.nombre} fue actualizado con exito`,'Almacen Actualizado'); 
+    if (this.id != 0) {
+      // modificar
+      almacen.id = this.id;
+      this._almacenServices.UpdateAlmacen(this.id, almacen).subscribe(() => {
+        this.toastr.success(`El Proveedor ${almacenData.nombre} fue actualizado con exito`, 'Almacen Actualizado');
         this.router.navigate(['home/almacen']);
       })
-    }else{
-      //agregar
-      this._almacenServices.newAlmacen(almacen).subscribe(()=>{
-        this.toastr.success(`El Proveedor ${almacenData.nombre} fue registrado con exito`,'Almacen Registrado');
+    } else {
+      // agregar
+      this._almacenServices.newAlmacen(almacen).subscribe(() => {
+        this.toastr.success(`El Proveedor ${almacenData.nombre} fue registrado con exito`, 'Almacen Registrado');
         this.router.navigate(['home/almacen']);
       })
     }
-    
-
   }
 
-  getAlmacen(id:number){
-    this._almacenServices.getAlmacen(id).subscribe((data:Almacen)=>{
-      //console.log(data);
-      this.AlmacenForm = this.fb.group({
-        nombre: [data.nombre],
-        direccion: [data.direccion],
-        ciudad: [data.ciudad],
-        capacidad_actual: [data.capacidad_actual],
-        capacidad_total: [data.capacidad_total]
+  getAlmacen(id: number) {
+    this._almacenServices.getAlmacen(id).subscribe((data: Almacen) => {
+      this.AlmacenForm.setValue({
+        nombre: data.nombre,
+        direccion: data.direccion,
+        ciudad: data.ciudad,
+        capacidad_actual: data.capacidad_actual,
+        capacidad_total: data.capacidad_total
       });
     })
   }
 
-  navegar(){
+  navegar() {
     this.router.navigate(['/home/almacen']);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { LoginComponent } from './login/login.component';
 import { MenuComponent } from './menu/menu.component';
-import { CommonModule } from '@angular/common';
-//Modulos
-import { HttpClientModule,HTTP_INTERCEPTORS } from '@angular/common/http';
-import { ToastrModule } from 'ngx-toastr';
-
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { VenderComponent } from './vender/vender.component';
 import { ProveedorComponent } from './proveedor/proveedor.component';
 import { LayoutComponent } from './layout/layout.component';
@@ -22,10 +18,8 @@ import { InventarioComponent } from './inventario/inventario.component';
 import { SpinnerComponent } from './shared/spinner/spinner.component';
 import { SignInComponent } from './new-User/sign-in.component';
 import { NewPasswordComponent } from './new-password/new-password.component';
-
 import { AddNewProveedorComponent } from './add-new-proveedor/add-new-proveedor.component';
 import { AddNewAlmacenComponent } from './add-new-almacen/add-new-almacen.component';
-
 
 @NgModule({
   declarations: [
@@ -48,11 +42,9 @@ import { AddNewAlmacenComponent } from './add-new-almacen/add-new-almacen.compon
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    CommonModule,
     ReactiveFormsModule,
     FormsModule,
     BrowserAnimationsModule,
-    HttpClientModule,
     ToastrModule.forRoot({
       timeOut: 4000,
       positionClass: 'toast-bottom-right',
